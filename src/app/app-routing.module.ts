55ff2f8739fb1bbb90a6081a141cea9c625a9abd
Igor Ilchenko <wog516@gmail.com>
2020-10-09 12:02:43 +0300

    added start and google login pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'start',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'start',
+    loadChildren: () => import('./pages/start/start.module').then( m => m.StartPageModule)
+  },
+  {
+    path: 'google-login',
+    loadChildren: () => import('./pages/google-login/google-login.module').then( m => m.GoogleLoginPageModule)
   }
 ];
 
