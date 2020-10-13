9be7bd1d2aa1b63f2c5b4c49652864d070d845f9
Igor Ilchenko <wog516@gmail.com>
2020-10-13 11:36:39 +0300

    added translations for email registration page

--- src/app/pages/login/email-registration/email-registration.module.ts
+++ src/app/pages/login/email-registration/email-registration.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { EmailRegistrationPageRoutingModule } from './email-registration-routing.module';
 
 import { EmailRegistrationPage } from './email-registration.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    EmailRegistrationPageRoutingModule
+    EmailRegistrationPageRoutingModule,
+    TranslateModule
   ],
   declarations: [EmailRegistrationPage]
 })
