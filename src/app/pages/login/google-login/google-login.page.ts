88af4221e03010842fdea3b3ac6ba444e6c462a0
Igor Ilchenko <wog516@gmail.com>
2020-11-04 11:02:08 +0200

    added validation to google login

--- src/app/pages/login/google-login/google-login.page.ts
+++ src/app/pages/login/google-login/google-login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PlatformService } from '../../../services/platform.service';
 import { TranslateService } from '@ngx-translate/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoggerService } from '../../../services/logger.service';
 
 @Component({
   selector: 'app-google-login',
@@ -9,23 +11,31 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class GoogleLoginPage implements OnInit {
   android = this.platformService.android;
-  continueBtnLabel: string;
   btnRouterLink = '/location-setting';
+  continueBtnLabel: string;
+  loginWith: string;
+  myForm: FormGroup;
+
 
   constructor(
     private translate: TranslateService,
-    private platformService: PlatformService
+    private platformService: PlatformService,
+    private formBuilder: FormBuilder,
+    private logger: LoggerService
   ) { }
 
   ngOnInit() {
+    this.loginWith = this.platformService.loginWith;
     this.getBtnLabel();
-  }
-
-  continue() {
+    this.myForm = this.formBuilder.group({
+      country: ['Germany'],
+      checkbox1: [false, [Validators.required]],
+      checkbox2: [false, [Validators.requiredTrue]],
+    });
   }
 
   getBtnLabel() {
-    switch (this.platformService.loginWith) {
+    switch (this.loginWith) {
       case 'md':
         this.continueBtnLabel = this.translate.instant('start.googleBtn');
         break;
@@ -38,5 +48,7 @@ export class GoogleLoginPage implements OnInit {
         break;
     }
   }
+
+  continue() {}
 
 }
--- src/app/services/platform.service.ts
+++ src/app/services/platform.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PlatformService {
-  android = false;
+  android = true;
   loginWith: string;
 
   constructor() { }
