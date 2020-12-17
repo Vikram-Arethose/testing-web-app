229f223d85226e595fe52554748fc5eb64c4ea92
Igor Ilchenko <wog516@gmail.com>
2020-12-17 09:50:43 +0200

    fixed favorites  bakery item

--- src/app/pages/menu/favorites/favorites.page.ts
+++ src/app/pages/menu/favorites/favorites.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HomeBranch } from '../../../models/http/homeBranch';
 import { HttpService } from '../../../services/http.service';
+import { BakeryService } from '../../../services/bakery.service';
 
 @Component({
   selector: 'app-favorites',
@@ -11,13 +12,23 @@ import { HttpService } from '../../../services/http.service';
 export class FavoritesPage implements OnInit {
 
   bakeries: Observable<HomeBranch[]>;
+  today: string;
 
   constructor(
+    public bakeryServ: BakeryService,
     private httpServ: HttpService
   ) { }
 
   ngOnInit() {
     this.bakeries = this.httpServ.getFavorites();
+    this.setToday();
+  }
+
+  setToday() {
+    const date = new Date();
+    const day = date.getDay();
+    const weekDays: string[] = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+    this.today = weekDays[day];
   }
 
   onFavorite(bakeryId: number, $event) {
