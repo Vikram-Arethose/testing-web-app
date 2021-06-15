4d4965768e8d31664e4de2a0967c6fcafea59408
Igor Ilchenko <wog516@gmail.com>
2021-06-15 17:38:25 +0300

    fix android git files

---
+++ android/app/src/main/java/com/brotchen/app/MainActivity.java
@@ -0,0 +1,24 @@
+package com.brotchen.app;
+import com.getcapacitor.community.firebaseanalytics.FirebaseAnalytics;
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+
+import java.util.ArrayList;
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Initializes the Bridge
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // Additional plugins you've installed go here
+        add(GoogleAuth.class);
+        add(com.getcapacitor.community.facebooklogin.FacebookLogin.class);
+        add(FirebaseAnalytics.class);
+    }});
+  }
+}
