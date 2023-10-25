726c9333e89cae5c7bad51644426f79231fe9b62
ivan2345 <39767175+ivan2345@users.noreply.github.com>
2023-10-25 13:07:08 +0200

    add GoogleMapsModule to bakery-search module

--- src/app/pages/bakery-search/bakery-search.module.ts
+++ src/app/pages/bakery-search/bakery-search.module.ts
@@ -9,10 +9,11 @@ import { BakerySearchPageRoutingModule } from './bakery-search-routing.module';
 import { BakerySearchPage } from './bakery-search.page';
 import { TranslateModule } from '@ngx-translate/core';
 import { ComponentsModule } from '../../components/components.module';
-
+import { GoogleMapsModule } from '@angular/google-maps'
 @NgModule({
   imports: [
     CommonModule,
+    GoogleMapsModule,
     FormsModule,
     IonicModule,
     BakerySearchPageRoutingModule,
