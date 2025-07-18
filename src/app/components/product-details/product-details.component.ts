47ece0933b07f08ed0792c82008ed50a59198f8e
Mahendra Raguri <ragurimahendra@gmail.com>
2025-07-18 17:46:22 +0530

    Product Details Page Availabity change for Dilvery

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { ModalController } from '@ionic/angular';
 import { Product } from '../../models/http/bakeryFull';
 import { LoggerService } from '../../services/logger.service';
 import { TranslateService } from '@ngx-translate/core';
+import { BakeryService } from 'src/app/services/bakery.service';
 
 @Component({
   selector: 'app-product-details',
@@ -13,19 +14,33 @@ export class ProductDetailsComponent implements OnInit {
 
   @Input() product: Product;
 
-  get availability(): string {
-    if (this.product.availability_new.length === 7) {
+  get availability(): string { 
+         let bakeryDetails
+         let isDelveryAvailable
+      this.bakeryService.bakeryDetails$.subscribe(details => {
+      bakeryDetails = details;
+      if(bakeryDetails && bakeryDetails.delivery && bakeryDetails.delivery == 1)isDelveryAvailable = true;
+      console.log('bakeryDetails: ', bakeryDetails);
+    });
+        let takeAwayOrDeliveryAvailability = isDelveryAvailable ? this.product?.deliver_availability : this.product?.availability_new;
+    if (takeAwayOrDeliveryAvailability?.length === 7) {
       return this.translate.instant('productDetails.availableEveryDay');
     } else {
-      const availableDaysLabels = this.product.availability_new.map(item => item.label);
-      return this.translate.instant('productDetails.only') + ' ' + availableDaysLabels.join(', ');
+      if(takeAwayOrDeliveryAvailability == null || takeAwayOrDeliveryAvailability?.length === 0) {
+        return this.translate.instant('productDetails.notAvailable');
+      }else{
+        const availableDaysLabels = takeAwayOrDeliveryAvailability?.map(item => item.label);
+        return this.translate.instant('productDetails.only') + ' ' + availableDaysLabels?.join(', ');
+      }
     }
   }
 
   constructor(
     private modalController: ModalController,
     private logger: LoggerService,
-    private translate: TranslateService
+    private translate: TranslateService,
+    private bakeryService: BakeryService
+    
   ) { }
 
   ngOnInit() {
